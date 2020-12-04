72c0b7d085f68c6e2623a776255fae62036d5536
Duncan <duncanjosephwatts@gmail.com>
2020-12-04 14:52:03 +0100

    Working solution

--- prob4.f90
+++ prob4.f90
@@ -1,13 +1,13 @@
 program main
    implicit none
    integer, dimension(1000)  :: expenses=0
-   integer                   :: prod, i, j, k, status, ioerror
+   integer                   :: prod, i, j, k, status, ioerror, n
    character(len=200)         :: msg, err_string
    character(len=5)          :: ind_1, ind_2
 
 
-   integer                   :: n_valid=0
-   integer                   :: byr=0, iyr=0, eyr=0, hgt=0, hcl=0, ecl=0, pid=0, cid=0
+   integer                   :: n_valid=0, n_invalid=0
+   integer                   :: byr=0, iyr=0, eyr=0, hgt=0, hcl=0, ecl=0, pid=0, cid=0, s
    character(len=1)          :: c
    character(:), allocatable :: password
 
@@ -20,22 +20,8 @@ program main
    fileopen: if (status == 0) then
       do
          read(9, '(A)', iostat=status) msg
-         if (status /= 0) exit
-         write(*,*) msg
-         if  (len(trim(msg)) == 0) then
-             !write(*,*) byr, iyr, eyr, hgt, hcl, ecl, pid, cid
-             if (cid == 0) then
-                 if (byr+iyr+eyr+hgt+hcl+ecl+pid == 7) then
-                     n_valid = n_valid + 1
-                 end if
-                 write(*,*) 'valid'
-             else if (byr+iyr+eyr+hgt+hcl+ecl+pid+cid == 8) then
-                 n_valid = n_valid + 1
-                 write(*,*) 'valid'
-             else
-                 write(*,*) 'invalid'
-             end if
-             !write(*,*) 'New passport'
+         if (len(trim(msg)) == 0) then
+             s = byr+iyr+eyr+hgt+hcl+ecl+pid
              byr=0
              iyr=0
              eyr=0
@@ -43,30 +29,77 @@ program main
              hcl=0
              ecl=0
              pid=0
+             if (cid == 0) then
+                 if (s == 7) then
+                     n_valid = n_valid + 1
+                 else
+                     n_invalid = n_invalid + 1
+                 end if
+             else if (s == 7) then
+                 n_valid = n_valid + 1
+             else if (s == 0) then
+             else
+                 n_invalid = n_invalid + 1
+             end if
              cid=0
              j=0
          end if
+         if (status /= 0) exit
          do
             k = index(msg, ':')
             if (k == 0 ) exit
-            !write(*,*) msg(k-3:k-1)
             select case (msg(k-3:k-1))
                case ('byr')
-                   byr=1
+                  read(msg(k+1:k+5),*) n
+                  if (n .ge. 1920 .and. n .le. 2002) then
+                     byr=1
+                  end if
                case ('iyr')
-                   iyr=1
+                  read(msg(k+1:k+5),*) n
+                  if (n .ge. 2010 .and. n .le. 2020) then
+                     iyr=1
+                  end if
                case ('eyr')
-                   eyr=1
+                  read(msg(k+1:k+5),*) n
+                  if (n .ge. 2020 .and. n .le. 2030) then
+                     eyr=1
+                  end if
                case ('hgt')
-                   hgt=1
+                  j = index(msg(k+1:k+6), 'in')
+                  if (j .ne. 0) then
+                     read(msg(k+1:k+j-1),*) n
+                     if (n .ge. 59 .and. n .le. 76) then
+                        hgt=1
+                     end if
+                  end if
+                  j = index(msg(k+1:k+6), 'cm')
+                  if (j .ne. 0) then
+                     read(msg(k+1:k+j-1),*) n
+                     if (n .ge. 150 .and. n .le. 193) then
+                        hgt=1
+                     end if
+                  end if
                case ('hcl')
-                   hcl=1
+                   if (msg(k+1:k+1) == '#') then
+                       j = index(msg(k+1:k+7), ' ')
+                       if (j == 0) then
+                          hcl=1
+                       end if
+                   end if
                case ('ecl')
-                   ecl=1
+                  j = index('amb blu brn gry grn hzl oth ', msg(k+1:k+4))
+                  if (j == 0) then
+                     ecl=0
+                  else
+                     ecl=1
+                  end if
                case ('pid')
-                   pid=1
+                  n = index(msg(k+1:k+11), ' ')
+                  if (n == 10) then
+                     pid=1
+                  end if
                case ('cid')
-                   cid=1
+                  cid=1
 
             end select
             msg(:k+1) = ' '
