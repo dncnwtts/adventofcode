6e5bc3ff33f88951d7026d691965f6ea4aa4a0a3
Duncan <duncanjosephwatts@gmail.com>
2020-12-08 19:34:54 +0100

    Working Fortran solution

--- prob7.f90
+++ prob7.f90
@@ -1,17 +1,17 @@
 program main
    use hashtbl
    implicit none
-   integer                   :: prod, i, j, k, l, status, istat, ioerror, nvals=0, n=0
-   character(len=88)         :: msg, container, contents, line
-   character(len=88)         :: bag, root, bags
-   character(len=10)         :: err_string
-   character(len=88), allocatable, dimension(:) :: a
-   character(len=88), allocatable, dimension(:) :: containers
+   integer, parameter        :: tbl_length = 1000, charlen=120
+   integer                   :: i, j, k, status, ioerror, nvals=0, n=0, success=0
+   character(len=charlen)         :: msg, container, line
+   character(len=charlen)         :: bag,  bags
+   character(len=charlen)         :: err_string
+   character(len=charlen), allocatable, dimension(:) :: a
+   character(len=charlen), allocatable, dimension(:) :: containers
    character(len=100000) :: baglist
    character(len=:), allocatable :: out
    
    type(hash_tbl_sll)        :: table
-   integer, parameter        :: tbl_length = 1000
 
 
    call table%init(tbl_length)
@@ -22,7 +22,7 @@ program main
         baglist(i:i) = ' '
    end do
 
-   open (unit=9, file='data/dummy_input7.txt', status='OLD', action='READ', &
+   open (unit=9, file='data/input7.txt', status='OLD', action='READ', &
            iostat=ioerror, iomsg=err_string)
 
    fileopen: if (status == 0) then
@@ -30,7 +30,6 @@ program main
          read(9, '(A)', iostat=status) msg
          if (status /= 0) exit
          nvals = nvals + 1
-
       end do
 
       ! allocate memory
@@ -45,90 +44,141 @@ program main
       do i=1, nvals
          read(9, '(A)',iostat=status) a(i)
       end do
+
+      do i=1, nvals
+          line = a(i)
+          j = index(line, 'contain')
+          k = index(line, 'bag')
+          container = line(1:k-1)
+          bags = line(j+len('contain'):)
+          call table%put(key=trim(container), val=bags)
+
+          containers(i) = container
+
+          call table%get(key=trim(container), val=out)
+      end do
+
+      do i =1, nvals
+         call is_in_table(containers(i), table, n, success)
+      end do
+      write(*,*) n
+
+
+      n = 0
+      bag = 'shiny gold'
+      call num_in_table(bag, table,  n, success)
+      write(*,*) n
+
+
+      if (allocated(a)) deallocate(a)
+
+      call table%free
    else fileopen
       write(*,*) 'oops'
    end if fileopen
 
-   do i=1, nvals
-       !write(*,*) a(i), i, nvals
-       !call is_in_tree(bag, a(i), i, a, baglist, n)
-       line = a(i)
-       j = index(line, 'contain')
-       k = index(line, 'bag')
-       container = line(1:k-1)
-       bags = line(j+len('contain'):)
-       call table%put(key=trim(container), val=bags)
-
-       containers(i) = container
-
-       call table%get(key=trim(container), val=out)
-       write(*,*) trim(container)
-       write(*,*) out
-   end do
-   write(*,*) n
-
-
-   do i =1, nvals
-      call is_in_table(containers(i), table, n)
-   end do
-
-
-   if (allocated(a)) deallocate(a)
-
-   call table%free
-
 contains
 
 
-    recursive subroutine is_in_table(bag, table, n_out)
+    recursive subroutine is_in_table(bag, table, n_out, success)
         implicit none
-        character(len=88), intent(in)    :: bag
-        type(hash_tbl_sll), intent(in)   :: table
+        character(len=charlen), intent(in)    :: bag
+        type(hash_tbl_sll), intent(inout)   :: table
         integer, intent(inout) :: n_out
-        character(len=88)  :: bags, msg, container, contents, line
+        integer, intent(out) :: success
+        character(len=charlen)  :: bags=' ', container=' ', next
         character(len=:), allocatable :: out
 
-        integer :: i, j, k, l, m, n
 
-        write(*,*) 't6', bag
-        write(*,*) 't7', ' calling table ', trim(bag)
+        integer :: l, m
+
+        success = 0
+
+        if (trim(bag) == 'shiny gold') return
+
         call table%get(key=trim(bag), val=out)
-        write(*,*) trim(bag), out
+        bags = out
 
-        j = index(out, 'no other')
-        if (j .ne. 0) then
-            write(*,*) 'Found no shiny golds'
-            return
+
+        if  (index(bags, 'shiny gold') .ne. 0) then
+            n_out = n_out + 1
+            success = 1
+        else
+           do
+               if (len(trim(bags)) == 0) exit
+               if (index(bags, 'no other') .ne. 0) then
+                   exit
+               end if
+               if (index(bags, 'shiny gold') .ne. 0) then
+                   n_out = n_out + 1
+                   success = 1
+                   exit
+               end if
+               l = index(bags, ',')
+               if (l .ne. 0) then
+                   next = trim(bags(l+1:))
+               else
+                   next = ' '
+               end if
+               m = index(bags, 'bag')
+               container = trim(bags(4:m-1))
+               call is_in_table(container, table,n_out, success)
+               if (success == 1) then
+                   exit
+               end if
+               bags = next
+           end do
         end if
-        j = index(out, 'shiny gold')
-        if (j .ne. 0) then
-            n = n + 1
-            write(*,*) 'Found one!'
-            return
-        end if
-
-        do
-            l = index(out, ',')
-            m = index(out, 'bag')
-            write(*,*) 't1',out(3:m-1)
-            if (l == 0) then
-                m = index(out, 'bag')
-                bags = out(3:m-1)
-                write(*,*) 't3',bags
-                call is_in_table(bags, table, n_out)
-                exit
-            else
-                write(*,*) 't4', out(1:l-1), out(l+1:)
-                m = index(out(1:l), 'bag')
-                bags = trim(out(3:m-1))
-                write(*,*) 't5', bags
-                call is_in_table(bags, table, n_out)
-                out = out(l+1:)
-            end if
-
-        end do
-
-        
     end subroutine is_in_table
+
+    recursive subroutine num_in_table(bag, table, n_out, success)
+        implicit none
+        character(len=charlen), intent(in)    :: bag
+        type(hash_tbl_sll), intent(inout)   :: table
+        integer, intent(inout) :: n_out
+        integer, intent(out) :: success
+        character(len=charlen)  :: bags=' ', container=' ', next
+        character(len=:), allocatable :: out
+
+
+        integer :: i, j, m, n, n_containers
+
+
+        n_containers = 0
+
+
+        success = 0
+
+
+        call table%get(key=trim(bag), val=out)
+        bags = out
+
+
+
+        if (index(bags, 'no other') .eq.  0) then
+           sub_bags: do
+               m = 0
+               if (len(trim(bags)) .eq. 0) exit
+               i = index(bags, ',')
+               if (i .ne. 0) then
+                   next = trim(bags(i+1:))
+               else
+                   next = ' '
+               end if
+               j = index(bags, 'bag')
+               container = trim(bags(4:j-1))
+               read(bags(1:3),*) n
+               call num_in_table(container, table, m, success)
+               if (m == 0) then
+                   n_out = n_out + n
+               else
+                   n_out = n_out + (m+1)*n
+               end if
+               bags = next
+           end do sub_bags
+       end if
+
+
+    end subroutine num_in_table
 
 end program main
