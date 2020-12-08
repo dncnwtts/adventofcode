27075477849df5b559baabaa5ecbe4c956fcd1a8
Duncan <duncanjosephwatts@gmail.com>
2020-12-08 12:48:04 +0100

    Newer version with hash tables

--- prob7.f90
+++ prob7.f90
@@ -1,18 +1,21 @@
 program main
+   use hashtbl
    implicit none
    integer                   :: prod, i, j, k, l, status, istat, ioerror, nvals=0, n=0
    character(len=88)         :: msg, container, contents, line
    character(len=88)         :: bag, root, bags
    character(len=10)         :: err_string
    character(len=88), allocatable, dimension(:) :: a
+   character(len=88), allocatable, dimension(:) :: containers
    character(len=100000) :: baglist
+   character(len=:), allocatable :: out
+   
+   type(hash_tbl_sll)        :: table
+   integer, parameter        :: tbl_length = 1000
 
-   type :: node
-       character(len=88) :: v
-       type (node), pointer  :: p
-   end type
 
-   type(node), pointer :: head, ptr, tail
+   call table%init(tbl_length)
+
 
    bag = 'shiny gold'
    do i = 1, len(baglist)
@@ -28,30 +31,11 @@ program main
          if (status /= 0) exit
          nvals = nvals + 1
 
-         if (.not. associated(head)) then
-             write(*,*) 'ok'
-             allocate (head)
-             tail => head
-             nullify (tail%p)
-             tail%v = msg
-         else 
-             write(*,*) 'not ok'
-             allocate (tail%p)
-             tail => tail%p
-             nullify (tail%p)
-             tail%v = msg
-         end if
       end do
-
-      ptr => head
-      output: do
-        if (.not. associated(ptr)) exit
-        write (*,*) ptr%v
-        ptr => ptr%p
-      end do output
 
       ! allocate memory
       allocate( a(nvals), stat=status)
+      allocate( containers(nvals), stat=status)
 
       if (status == 0) then
           rewind( unit = 9)
@@ -65,76 +49,86 @@ program main
       write(*,*) 'oops'
    end if fileopen
 
-   line = a(1)
-   i = index(line, 'contain')
-   container = line(1:i-2)
-   bags = line(i+len('contain'):)
-
    do i=1, nvals
        !write(*,*) a(i), i, nvals
-       call is_in_tree(bag, a(i), i, a, baglist, n)
+       !call is_in_tree(bag, a(i), i, a, baglist, n)
+       line = a(i)
+       j = index(line, 'contain')
+       k = index(line, 'bag')
+       container = line(1:k-1)
+       bags = line(j+len('contain'):)
+       call table%put(key=trim(container), val=bags)
+
+       containers(i) = container
+
+       call table%get(key=trim(container), val=out)
+       write(*,*) trim(container)
+       write(*,*) out
    end do
-   !write(*,*) trim(baglist)
    write(*,*) n
+
+
+   do i =1, nvals
+      call is_in_table(containers(i), table, n)
+   end do
 
 
    if (allocated(a)) deallocate(a)
 
+   call table%free
+
 contains
 
 
-    recursive subroutine is_in_tree(bag, root, start_int, a, baglist, n_out)
+    recursive subroutine is_in_table(bag, table, n_out)
         implicit none
-        character(len=88)             :: bag, root, bags
-        integer, intent(in) :: start_int
+        character(len=88), intent(in)    :: bag
+        type(hash_tbl_sll), intent(in)   :: table
         integer, intent(inout) :: n_out
-        character(len=100000), intent(inout) :: baglist
-        character(len=88)         :: msg, container, contents, line
-        character(len=88), intent(in), dimension(:) :: a
+        character(len=88)  :: bags, msg, container, contents, line
+        character(len=:), allocatable :: out
 
         integer :: i, j, k, l, m, n
 
-        line = a(start_int)
-        
-        i = index(line, 'contain')
-        container = line(1:i-2)
-        bags = line(i+len('contain'):)
-        !write(*,*) trim(container)
-        !write(*,*) trim(bags)
-        j = index(bags, 'no other')
+        write(*,*) 't6', bag
+        write(*,*) 't7', ' calling table ', trim(bag)
+        call table%get(key=trim(bag), val=out)
+        write(*,*) trim(bag), out
+
+        j = index(out, 'no other')
         if (j .ne. 0) then
+            write(*,*) 'Found no shiny golds'
             return
         end if
-        k = index(bags, trim(bag))
-        if (k .ne. 0) then
-            !write(*,*) 'Found ', trim(bag), ' in question, in ', trim(container), trim(root)
-            ! Update baglist
-            l = index(baglist, trim(root))
-            if (l == 0) then
-                l = index(baglist, '       ')
-                baglist(l+1:len(trim(root))+l+1) = trim(root)
-                n_out = n_out + 1
-            end if
+        j = index(out, 'shiny gold')
+        if (j .ne. 0) then
+            n = n + 1
+            write(*,*) 'Found one!'
             return
         end if
 
-
-        ! do loop here, exit when no commas are left.
         do
-            l = index(bags, ',')
-            !write(*,*) trim(bags(:l-1)), trim(bags(l+1:))
+            l = index(out, ',')
+            m = index(out, 'bag')
+            write(*,*) 't1',out(3:m-1)
             if (l == 0) then
-                m = index(bags, '     ')
-                do n = start_int+1, nvals
-                   call is_in_tree(bag, root, n, a, baglist, n_out)
-                end do
+                m = index(out, 'bag')
+                bags = out(3:m-1)
+                write(*,*) 't3',bags
+                call is_in_table(bags, table, n_out)
                 exit
             else
-                bags = bags(l+1:)
+                write(*,*) 't4', out(1:l-1), out(l+1:)
+                m = index(out(1:l), 'bag')
+                bags = trim(out(3:m-1))
+                write(*,*) 't5', bags
+                call is_in_table(bags, table, n_out)
+                out = out(l+1:)
             end if
 
         end do
 
-    end subroutine is_in_tree
+        
+    end subroutine is_in_table
 
 end program main
