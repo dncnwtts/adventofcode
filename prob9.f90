101f0fc8e545ee0b470005710f89a42062836504
Duncan <duncanjosephwatts@gmail.com>
2020-12-09 12:42:36 +0100

    Working part 1

---
+++ prob9.f90
@@ -0,0 +1,53 @@
+program main
+   implicit none
+   integer                   :: i, j, k, l, buff, status, ioerror
+   integer                   :: ind=1, nvals=0, acc=0
+   character(len=88)         :: msg
+   character(len=10)         :: err_string
+   integer, allocatable, dimension(:) :: a
+   integer, allocatable, dimension(:) :: counts, ls, hs
+
+   open (unit = 9, file = 'data/input9.txt', status = 'OLD', action = 'READ', &
+           iostat = ioerror, iomsg = err_string)
+
+   fileopen: if (status == 0) then
+      do
+         read(9, '(A)', iostat = status) msg
+         if (status /= 0) exit
+         nvals = nvals + 1
+      end do
+
+      ! allocate memory
+      allocate( a(nvals), stat = status)
+      allocate( counts(nvals), stat = status)
+      counts = 0
+      if (status == 0) then
+          rewind( unit = 9)
+      else
+          write(*,*) status
+      end if
+      do i = 1, nvals
+         read(9, *, iostat = status) a(i)
+      end do
+
+      buff = 5
+      outer: do i=buff+1, nvals
+          do j=i-buff, i-1
+              do k=j+1, i-1
+                  if (a(i) ==  a(j)+a(k)) then
+                      counts(i) = 1
+                  end if
+              end do
+          end do
+      end do outer
+
+      i = minloc(counts(buff+1:), 1)+buff
+      write(*,*) a(i)
+
+      deallocate(a, counts)
+
+   else fileopen
+      write(*,*) 'file not opening'
+   end if fileopen
+
+end program main
