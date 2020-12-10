5f45666ff9920767cfe9e4e911bf3d351342685c
Duncan <duncanjosephwatts@gmail.com>
2020-12-10 16:39:20 +0100

    Working part 2

--- prob10.f90
+++ prob10.f90
@@ -1,11 +1,11 @@
 program main
    use sorts
    implicit none
-   integer                   :: i, j, k, l, buff, status, ioerror, answer1
+   integer                   :: i, status, ioerror
    integer                   :: nvals=0, jumps1=0, jumps3=0, n=0, m=0
    character(len=88)         :: msg
    character(len=10)         :: err_string
-   integer, allocatable, dimension(:) :: a,b,jumps
+   integer, allocatable, dimension(:) :: a, jumps
 
    open (unit = 9, file = 'data/input10.txt', status = 'OLD', action = 'READ', &
            iostat = ioerror, iomsg = err_string)
@@ -45,6 +45,24 @@ program main
       end do
 
       write(*,*) jumps1*jumps3
+
+
+      m = 1
+      n = 1
+      do i = 1, nvals+1
+        if (jumps(i) == 3) then
+          if (i-m > 1) then
+            ! write(*,*) jumps(m:i)
+            print "(8i1)", jumps(m:i)
+          end if
+          m = i+1
+        end if
+      end do
+
+      write(*,*) 
+
+      write(*,*) 'There is a code of differences: 11113 -> 7, 1113 -> 4, 113 -> 2'
+      write(*,*) 'Multiply these all together to get the answer.'
 
 
       deallocate(a)
